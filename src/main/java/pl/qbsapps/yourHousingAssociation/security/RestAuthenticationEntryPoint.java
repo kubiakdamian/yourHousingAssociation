7d9975052604f0d106806272c5da7283e7f3c647
Damian Kubiak <kubiak.damian96@gmail.com>
2019-03-15 23:44:29 +0100

    Changed unauthorized error message type from html to String

--- src/main/java/pl/qbsapps/yourHousingAssociation/security/RestAuthenticationEntryPoint.java
+++ src/main/java/pl/qbsapps/yourHousingAssociation/security/RestAuthenticationEntryPoint.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -20,7 +19,10 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
         logger.error("Responding with unothorized error. Message - {}", e.getMessage());
 
-        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-                "Sorry, You are not authorized to access this resource");
+        httpServletResponse.resetBuffer();
+        httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        httpServletResponse.setHeader("Content-Type", "application/JSON");
+        httpServletResponse.getOutputStream().print("Sorry, You are not authorized to access this resource");
+        httpServletResponse.flushBuffer();
     }
 }
